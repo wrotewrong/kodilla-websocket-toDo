b34eb77846179b7a3962f9cd89bc7978bb6f8e8a
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-01-22 21:26:53 +0100

    Add websocket to the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,37 @@
 const App = () => {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className='App'>
+      <header>
+        <h1>ToDoList.app</h1>
+      </header>
+
+      <section className='tasks-section' id='tasks-section'>
+        <h2>Tasks</h2>
+
+        <ul className='tasks-section__list' id='tasks-list'>
+          <li className='task'>
+            Shopping <button className='btn btn--red'>Remove</button>
+          </li>
+          <li className='task'>
+            Go out with a dog <button className='btn btn--red'>Remove</button>
+          </li>
+        </ul>
+
+        <form id='add-task-form'>
+          <input
+            className='text-input'
+            autocomplete='off'
+            type='text'
+            placeholder='Type your description'
+            id='task-name'
+          />
+          <button className='btn' type='submit'>
+            Add
+          </button>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default App;
--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io');
+
+const tasks = [];
 
 app.use((req, res) => {
   res.status(404).send('Not found');
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('server is running on port 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', ({ task, id }) => {
+    tasks.push({ task, id });
+    socket.broadcast.emit('addTask', { user, id });
+  });
+
+  socket.on('removeTask', (id) => {
+    const index = tasks.findIndex((element) => element.id == id);
+    if (index != -1) {
+      tasks.splice(index, 1);
+    }
+    socket.broadcast.emit('removeTask', id);
+  });
 });
