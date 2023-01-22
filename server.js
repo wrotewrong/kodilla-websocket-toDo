abc5f49e4821bedcb7315097ee5f51660f7dd84a
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-01-22 19:58:45 +0100

    Add basic server and client code

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res) => {
+  res.status(404).send('Not found');
+});
+
 app.listen(8000, () => {
   console.log('server is running on port 8000');
 });
